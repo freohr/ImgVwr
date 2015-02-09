0599e9d963ea3185391f10499738e5ca0fa47ab4
Stephen <faure.stephen@gmail.com>
2015-02-09 18:42:00 +0100

    Classes de base pour Image* et TagEditor

---
+++ src/Editor/Image.java
@@ -0,0 +1,9 @@
+package Editor;
+
+import javax.swing.*;
+
+/**
+ * Created by stephen on 09/02/2015.
+ */
+public class Image extends JFrame{
+}
---
+++ src/Editor/Tag.java
@@ -0,0 +1,9 @@
+package Editor;
+
+import javax.swing.*;
+
+/**
+ * Created by stephen on 09/02/2015.
+ */
+public class Tag extends JFrame {
+}
