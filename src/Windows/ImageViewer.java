78f6dfae3c02dc8764aac518678233a9d61ef42e
Remy <remy.lherault@gmail.com>
2015-02-11 17:23:07 +0100

    début modif imageViewer.java

--- src/Windows/ImageViewer.java
+++ src/Windows/ImageViewer.java
@@ -6,10 +6,26 @@ import java.awt.*;
 /**
  * Created by stephen on 09/02/2015.
  */
+
+/**
+ * Modified by Rémy on 09/02/2015.
+ */
 public class ImageViewer extends JFrame{
 
     public ImageViewer() throws HeadlessException {
         this.setTitle("ImageViewer");
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        this.setSize(800, 600);
+
+        this.initViewer();
     }
+
+    private void initViewer() {
+
+
+    }
+
+
+
 }
