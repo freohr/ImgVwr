2158371d637f38505e1abe2060abc70c8f17809a
Remy <remy.lherault@gmail.com>
2015-03-11 14:35:33 +0100

    modif du tag editor

--- src/Windows/TagEditor.java
+++ src/Windows/TagEditor.java
@@ -19,29 +19,50 @@ public class TagEditor extends JFrame {
 
         this.setSize(this.getPreferredSize());
 
-        initTagWindow();
+        this.initTagWindow();
     }
 
-    private void initTagWindow() {
+    private JPanel initTagWindow() {
         JPanel sideBar = new JPanel(new GridBagLayout());
 
         GridBagConstraints c = new GridBagConstraints();
 
-        c.gridx = 0;
-        c.gridheight = 0;
-        c.gridwidth = 0;
+        c.gridx = c.gridy = 0;
+        c.gridheight = 10;
+        c.gridwidth = 10;
 
+
+        c.gridheight = 1;
+        c.gridwidth = 2;
+        c.gridx = 8;
+        c.gridy = 3;
         JButton addButton = new JButton();
         addButton.setText("Add a Tag");
         addButton.addActionListener(new addButtonListener());
 
+        sideBar.add(addButton, c);
+
+        c.gridheight = 1;
+        c.gridwidth = 2;
+        c.gridx = 8;
+        c.gridy = 4;
         JButton saveButton = new JButton();
         saveButton.setText("Save");
         saveButton.addActionListener(new saveButtonListener());
 
+        sideBar.add(saveButton, c);
+
+        c.gridheight = 1;
+        c.gridwidth = 2;
+        c.gridx = 8;
+        c.gridy = 5;
         JButton deleteButton = new JButton();
         deleteButton.setText("Deletes Tag");
         deleteButton.addActionListener(new deleteButtonListener());
+
+        sideBar.add(deleteButton, c);
+
+        return sideBar;
 
     }
 
