dfd3297cc82403463a0d0ae99ccf4eddeb1890ca
Remy <remy.lherault@gmail.com>
2015-03-15 11:46:59 +0100

    debut ajout tags dans tags editor

--- src/Windows/TagEditor.java
+++ src/Windows/TagEditor.java
@@ -9,6 +9,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+
 
 /**
  * Created by stephen on 09/02/2015.
@@ -98,6 +103,10 @@ public class TagEditor extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+            public String path;
+
+            //#PROGRESS remy
+            path = "";
 
         }
     }
